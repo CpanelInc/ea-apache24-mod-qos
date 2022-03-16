ca6eac5b7311c24959d88f4ee828d2d187cff87b
Julian Brown <julian.brown@cpanel.net>
2022-03-16 15:25:12 +0000

    ZC-9726: Added conf file

--- debify/debian/override_dh_auto_install.sh
+++ debify/debian/override_dh_auto_install.sh
@@ -6,3 +6,7 @@ set -x
 mkdir -p $DEB_INSTALL_ROOT$_httpd_moddir
 install apache2/${upstream_name}.so $DEB_INSTALL_ROOT$_httpd_moddir/
 
+echo "ROOT :$DEB_INSTALL_ROOT${_sysconfdir}:"
+mkdir -p $DEB_INSTALL_ROOT${_sysconfdir}/apache2/conf.modules.d/
+cp $SOURCE1 $DEB_INSTALL_ROOT${_sysconfdir}/apache2/conf.modules.d/
+
